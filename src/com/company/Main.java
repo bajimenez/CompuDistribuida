1d091904efc6467d193855c44e93dde013eff797
bajimenez <brayan.jimenez@epn.edu.ec>
2017-05-12 10:43:04 -0500

    creacion proyecto en IntelliJ

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+    public static int comprobar (String cadena1,String cadena2){
+        return 0;
+    }
+}
+
+
